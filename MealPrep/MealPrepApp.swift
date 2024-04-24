bfdb51bcbc1d04c6cc1e84a5fed3be58572f8225
giulee <giulietta.g.lee@student.uts.edu.au>
2024-04-24 10:37:08 +1000

    Initial Commit

---
+++ MealPrep/MealPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPrepApp.swift
+//  MealPrep
+//
+//  Created by Giulietta Lee on 24/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct MealPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
